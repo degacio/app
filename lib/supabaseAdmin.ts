44453da1fcd188ffb948d860ca749f6ca3ec9864
degacio <36391498+degacio@users.noreply.github.com>
2026-02-24 16:24:20 -0300

    Updated supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types/database'
 
+console.log("URL:", process.env.EXPO_PUBLIC_SUPABASE_URL)
+console.log("SERVICE KEY:", process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY)
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
 
