e2ef7071b3b7cc8751338833f0a6e985e00c485a
degacio <36391498+degacio@users.noreply.github.com>
2026-02-24 16:14:51 -0300

    Updated supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,1 +1,16 @@
-import { supabase } from '@/lib/supabase'
+import { createClient } from '@supabase/supabase-js'
+import { Database } from '@/types/database'
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
+
+// 🔥 Não quebrar o Metro se variável não existir
+export const supabaseAdmin =
+  supabaseUrl && supabaseKey
+    ? createClient<Database>(supabaseUrl, supabaseKey, {
+        auth: {
+          autoRefreshToken: false,
+          persistSession: false,
+        },
+      })
+    : null
